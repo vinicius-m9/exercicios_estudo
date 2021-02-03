51d2db258f6e5c7ee2a6d37cac7fede6c387507f
unknown <viniciusmachadoribeiro@hotmail.com>
2021-02-03 14:08:54 -0300

    arquivo ex_teste.c modificado

--- C/ex_teste/ex_teste.c
+++ C/ex_teste/ex_teste.c
@@ -1,7 +1,15 @@
+/*
+Implemente um algoritmo que imprima na tela a seguinte mensagem:
+
+Programacao
+Estruturada
+2021
+*/
+
 #include<stdio.h>
 
 void main(){
-	printf("Programação\n");
+	printf("Programacao\n");
     printf("Estruturada\n");
     printf("2021");
 }
