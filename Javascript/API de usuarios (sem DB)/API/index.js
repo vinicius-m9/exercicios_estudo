4826018f422c52c596d76384b0faade9b286c250
unknown <viniciusmachadoribeiro@hotmail.com>
2021-02-16 19:19:48 -0300

    melhoria no código/remocao de else's

--- Javascript/API de usuarios (sem DB)/API/index.js
+++ Javascript/API de usuarios (sem DB)/API/index.js
@@ -18,11 +18,11 @@ server.get('/users/:id', (req, res) => {
   const id = Number(req.params.id)
   const user = users.find(usr => usr.id === id)
 
-  if (user) {
-    res.json(user)
-  } else {
-    res.status(404).send()
+  if (!user) {
+    return res.status(404).send()
   }
+
+  res.json(user)
 })
 
 server.post('/users', (req, res) => {
@@ -30,46 +30,45 @@ server.post('/users', (req, res) => {
   const email = users.find(usr => usr.email === user.email)
 
   if (email) {
-    res.status(400).send()
-  } else {
-    user.id = users.length
-    const id = users.find(usr => usr.id === user.id)
-
-    if (id) {
-      user.id = users[users.length - 1].id + 1
-    }
-
-    users.push(user)
-    res.send('Usuario criado com sucesso!')
+    return res.status(400).send()
   }
+
+  user.id = users.length
+  const id = users.find(usr => usr.id === user.id)
+
+  if (id) {
+    user.id = users[users.length - 1].id + 1
+  }
+
+  users.push(user)
+  res.send('Usuario criado com sucesso!')
 })
 
 server.put('/users/:id', (req, res) => {
   const id = Number(req.params.id)
   const indexId = users.findIndex(usr => usr.id === id)
 
-  if (indexId !== -1) {
-    const userAlteration = req.body
-    const { name, email, password } = userAlteration
-
-    if (name) users[indexId].name = name
-    if (email) users[indexId].email = email
-    if (password) users[indexId].password = password
-
-    res.send('Usuario alterado com sucesso!')
-  } else {
-    res.status(404).send()
+  if (indexId === -1) {
+    return res.status(404).send()
   }
+  const userAlteration = req.body
+  const { name, email, password } = userAlteration
+
+  if (name) users[indexId].name = name
+  if (email) users[indexId].email = email
+  if (password) users[indexId].password = password
+
+  res.send('Usuario alterado com sucesso!')
 })
 
 server.delete('/users/:id', (req, res) => {
   const id = Number(req.params.id)
   const indexId = users.findIndex(usr => usr.id === id)
 
-  if (indexId !== -1) {
-    users.splice(indexId, 1)
-    res.send('Usuario deletado com sucesso!')
-  } else {
-    res.status(404).send()
+  if (indexId === -1) {
+    return res.status(404).send()
   }
+
+  users.splice(indexId, 1)
+  res.send('Usuario deletado com sucesso!')
 })
