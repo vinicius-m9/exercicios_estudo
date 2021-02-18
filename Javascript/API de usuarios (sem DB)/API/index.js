ab33e85155fb06fc58adfda117ba54c4a8ad9633
vinicius-m9 <viniciusmachadoribeiro@hotmail.com>
2021-02-17 21:52:02 -0300

    FileSystem implementado e codigo modularizado

--- Javascript/API de usuarios (sem DB)/API/index.js
+++ Javascript/API de usuarios (sem DB)/API/index.js
@@ -4,7 +4,49 @@ const server = express()
 server.use(express.json())
 
 const PORT = process.env.PORT || 3000
-const users = []
+
+const fs = require('fs')
+const fakeDB = fs.readFileSync('users.json')
+const users = JSON.parse(fakeDB)
+
+const getUser = id => {
+  return users.find(usr => usr.id === id)
+}
+
+const createUser = user => {
+  user.id = users.length
+  const id = users.find(usr => usr.id === user.id)
+
+  if (id) {
+    user.id = users[users.length - 1].id + 1
+  }
+
+  users.push(user)
+
+  const newUser = JSON.stringify(users, null, 2)
+  fs.writeFileSync('users.json', newUser)
+}
+
+const changeUser = (alteration, id) => {
+  const indexId = users.findIndex(usr => usr.id === id)
+  const { name, email, password } = alteration
+
+  if (name) users[indexId].name = name
+  if (email) users[indexId].email = email
+  if (password) users[indexId].password = password
+
+  const userAlteration = JSON.stringify(users, null, 2)
+  fs.writeFileSync('users.json', userAlteration)
+}
+
+const deleteUser = id => {
+  const indexId = users.findIndex(usr => usr.id === id)
+
+  users.splice(indexId, 1)
+
+  const userDeleted = JSON.stringify(users, null, 2)
+  fs.writeFileSync('users.json', userDeleted)
+}
 
 server.listen(PORT, () => {
   console.log(`Listening at http://localhost:${PORT}`)
@@ -16,7 +58,7 @@ server.get('/users', (req, res) => {
 
 server.get('/users/:id', (req, res) => {
   const id = Number(req.params.id)
-  const user = users.find(usr => usr.id === id)
+  const user = getUser(id)
 
   if (!user) {
     return res.status(404).send()
@@ -33,42 +75,31 @@ server.post('/users', (req, res) => {
     return res.status(400).send()
   }
 
-  user.id = users.length
-  const id = users.find(usr => usr.id === user.id)
-
-  if (id) {
-    user.id = users[users.length - 1].id + 1
-  }
-
-  users.push(user)
+  createUser(user)
   res.send('Usuario criado com sucesso!')
 })
 
 server.put('/users/:id', (req, res) => {
   const id = Number(req.params.id)
-  const indexId = users.findIndex(usr => usr.id === id)
+  const user = getUser(id)
+  const userAlteration = req.body
 
-  if (indexId === -1) {
+  if (!user) {
     return res.status(404).send()
   }
-  const userAlteration = req.body
-  const { name, email, password } = userAlteration
 
-  if (name) users[indexId].name = name
-  if (email) users[indexId].email = email
-  if (password) users[indexId].password = password
-
+  changeUser(userAlteration, id)
   res.send('Usuario alterado com sucesso!')
 })
 
 server.delete('/users/:id', (req, res) => {
   const id = Number(req.params.id)
-  const indexId = users.findIndex(usr => usr.id === id)
+  const user = getUser(id)
 
-  if (indexId === -1) {
+  if (!user) {
     return res.status(404).send()
   }
 
-  users.splice(indexId, 1)
+  deleteUser(id)
   res.send('Usuario deletado com sucesso!')
 })
